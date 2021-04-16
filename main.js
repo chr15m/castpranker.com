678dc1d3c89aa26b3b1e4cc0c0e0ab620af61332
Chris McCormick <chris@mccormick.cx>
2021-04-16 09:50:09 +0800

    Initial commit of basic working app.

---
+++ main.js
@@ -0,0 +1,31 @@
+console.log("script loaded");
+
+$(document).ready(function() {
+  var colorPalette = ['000000', 'FF9966', '6699FF', '99FF66', 'CC0000', '00CC00', '0000CC', '333333', '0066FF', 'FFFFFF'];
+  var forePalette = $('.fore-palette');
+  var backPalette = $('.back-palette');
+
+  for (var i = 0; i < colorPalette.length; i++) {
+    forePalette.append('<a href="#" data-command="forecolor" data-value="' + '#' + colorPalette[i] + '" style="background-color:' + '#' + colorPalette[i] + ';" class="palette-item"></a>');
+    backPalette.append('<a href="#" data-command="backcolor" data-value="' + '#' + colorPalette[i] + '" style="background-color:' + '#' + colorPalette[i] + ';" class="palette-item"></a>');
+  }
+
+  $('.toolbar a').click(function(e) {
+    console.log("hi");
+    var command = $(this).data('command');
+    if (command == 'h1' || command == 'h2' || command == 'p') {
+      console.log("one of these");
+      document.execCommand('formatBlock', false, command);
+    }
+    if (command == 'forecolor' || command == 'backcolor') {
+      document.execCommand($(this).data('command'), false, $(this).data('value'));
+    }
+    if (command == 'createlink' || command == 'insertimage') {
+      url = prompt('Enter the link here: ', 'http:\/\/');
+      document.execCommand($(this).data('command'), false, url);
+    } else {
+      console.log("hello");
+      document.execCommand($(this).data('command'), false, null);
+    }
+  });
+});
